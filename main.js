d20709406893d0e91695313219d284fd0b07e92c
shirotamaki <shiro.tamaki63@gmail.com>
2022-05-05 21:46:17 +0900

    Add: タイプライタ機能を追加

--- main.js
+++ main.js
@@ -4,22 +4,6 @@
 const { Select } = require('enquirer')
 const readline = require('readline')
 
-async function selectAdvise (name, answer) {
-  const obj = {
-    仕事: 'やり続けることしかないということだけですよ。自分の仕事にオレ才能あるんだと',
-    お金: `${name}の一生かかって稼ぐ金、YAZAWAの2秒。そこんとこ、ヨロシク！`,
-    恋愛: `アイ・ラブ・ユー オーケー?言っちゃえよ${name}！`,
-    人生: `${name}の人生なんだから。てめぇで走れ！`,
-    夢: 'ドアの向こうに夢があるなら、ドアが開くまで叩き続けるんだ！',
-    挑戦: '最近勝ち組とか負け組みとか流行っているけど、スタート切っているかどうかがボクは大事だと思うけどね',
-    挫折: '最初、サンザンな目にあう。二度目、オトシマエをつける。三度目、余裕',
-    幸せ: `アー・ユー・ハッピー？ ${name}はすでにハッピーなはずだぜ`,
-    老い: '年とるってのは細胞が老けることであって、魂が老けることじゃない',
-    相談したいことはない: 'ボクは別にいいんだけど、YAZAWAがなんて言うかな？'
-  }
-  return obj[answer]
-}
-
 async function firstMessage () {
   console.log('\n' +
 ' __   __     _      _____     _     __        __     _ \n' +
@@ -46,34 +30,47 @@ async function userNameInput () {
   })
 }
 
+function thankMessage (name) {
+  console.log(`\nサンキュー ${name} !! \n`)
+}
+
 async function selectWorry () {
   const prompt = new Select({
-    name: 'advisement', // 選択された答えのオブジェクト
-    message: 'YAZAWAに相談したい内容をセレクト！', // 説明文
-    choices: ['仕事', 'お金', '恋愛', '人生', '夢', '挑戦', '挫折', '幸せ', '老い', '相談したいことはない'] // 相談リスト
+    name: 'advisement',
+    message: 'YAZAWAに相談したい内容をセレクト！',
+    choices: ['仕事', 'お金', '恋愛', '人生', '夢', '挑戦', '挫折', '幸せ', '老い', 'とくにない']
   })
 
   return await prompt.run()
-    .then(choicedObj => {
-      const selectedWorry = choicedObj
-      return selectedWorry
+    .then( selectedWorry => {
+    return selectedWorry
     })
     .catch(console.error)
 }
 
+async function selectAdvise (name, answer) {
+  const obj = {
+    仕事: 'やり続けることしかないということだけですよ。自分の仕事にオレ才能あるんだと',
+    お金: `${name}の一生かかって稼ぐ金、YAZAWAの2秒。そこんとこ、ヨロシク！`,
+    恋愛: `アイ・ラブ・ユー オーケー？言っちゃえよ${name}！`,
+    人生: `${name}の人生なんだから。てめぇで走れ！`,
+    夢: 'ドアの向こうに夢があるなら、ドアが開くまで叩き続けるんだ！',
+    挑戦: '最近勝ち組とか負け組みとか流行っているけど、スタート切っているかどうかがボクは大事だと思うけどね',
+    挫折: '最初、サンザンな目にあう。二度目、オトシマエをつける。三度目、余裕',
+    幸せ: `アー・ユー・ハッピー？ ${name}はすでにハッピーなはずだぜ`,
+    老い: '年とるってのは細胞が老けることであって、魂が老けることじゃない',
+    とくにない: 'ボクは別にいいんだけど、YAZAWAがなんて言うかな？'
+  }
+  return obj[answer]
+}
+
 async function displayAdvice (selectedAdvice) {
-  console.log(
-    '-------------------------------------------------------------\n' +
-    ` ${selectedAdvice}\n` +
-    '--------------------------------------------------------------\n'
-  )
+  console.log('-------------------------------------------------------------')
+  await typewriter(selectedAdvice)
+  console.log('-------------------------------------------------------------')
 }
 
-async function thankMessage (name) {
-  console.log(`\nサンキュー ${name} !! \n`)
-}
-
-async function lastMessage () {
+function lastMessage () {
   console.log(
     '  ____    ___     ___    ____      _       _   _    ____   _  __ \n' +
     ' / ___|  / _ \\   / _ \\  |  _ \\    | |     | | | |  / ___| | |/ / \n' +
@@ -83,13 +80,25 @@ async function lastMessage () {
   )
 }
 
+function typewriter (text) {
+  let speed = 50
+  let displayText = text.split("")
+  displayText.forEach((char, index) => {
+    setTimeout(() => {
+      process.stdout.write(char)
+    }, speed * index)
+  })
+}
+
 async function main () {
   await firstMessage()
   const name = await userNameInput()
-  await thankMessage(name)
+  thankMessage(name)
   const selectedWorry = await selectWorry()
   const selectedAdvice = await selectAdvise(name, selectedWorry)
-  await displayAdvice(selectedAdvice)
+  // const displayText = typewriter(selectedAdvice)
+  const foo = await displayAdvice(selectedAdvice)
   await lastMessage()
 }
+
 main()
