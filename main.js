47b69908ee6b36cb65ad866c74c36b3ae2cdc3e2
shirotamaki <shiro.tamaki63@gmail.com>
2022-05-08 17:48:56 +0900

    Add: lastMessageメソッドにsetTimeoutを実装

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 'use strict'
 
-const keypress = require('keypress')
 const { Select } = require('enquirer')
 const readline = require('readline')
 
 class Display {
-  constructor () {
+  constructor (time) {
+    this.time = time
   }
 
-  async firstMessage () {
+  firstMessage () {
       console.log('\n' +
         ' __   __     _      _____     _     __        __     _ \n' +
         ' \\ \\ / /    / \\    |__  /    / \\    \\ \\      / /    / \\ \n' +
@@ -17,10 +17,10 @@ class Display {
         '   | |    / ___ \\   / /_   / ___ \\    \\ V  V /    / ___ \\ \n' +
         '   |_|   /_/   \\_\\ /____| /_/   \\_\\    \\_/\\_/    /_/   \\_\\ \n'
       )
-      console.log('キミの悩みにYAZAWAがロックなアドバイスをプレゼント!')
+      console.log('キミの悩みにYAZAWAがロックなアドバイスをするぜ!')
   }
 
-  async userNameInput () {
+  userNameInput () {
     const question = 'まずはキミの名前を教えてくれ！\nニックネームを入力してエンターでカモン！！\n'
     const inputData = readline.createInterface({
       input: process.stdin,
@@ -36,13 +36,13 @@ class Display {
   }
 
   thankMessage (name) {
-    console.log(`\nサンキュー ${name} !! \nイカした名前だな！！\n`)
+    console.log(`\nセンキュー ${name} !! \n`)
   }
 
   async selectWorry () {
     const prompt = new Select({
       name: 'advisement',
-      message: 'YAZAWAに相談したい内容をセレクト！',
+      message: 'YAZAWAに相談したい内容をセレクトしてくれ！',
       choices: ['仕事', 'お金', '恋愛', '人生', '夢', '挑戦', '挫折', '幸せ', '老い', 'とくにない']
     })
 
@@ -51,36 +51,29 @@ class Display {
     }).catch(console.error)
   }
 
-  async selectAdvise (name, answer) {
+  selectAdvise (name, advice) {
     const obj = {
-      仕事: 'やり続けることしかないということだけですよ。自分の仕事にオレ才能あるんだと',
-      お金: `${name}の一生かかって稼ぐ金、YAZAWAの2秒。そこんとこ、ヨロシク！`,
-      恋愛: `アイ・ラブ・ユー オーケー？言っちゃえよ${name}！`,
-      人生: `${name}の人生なんだから。てめぇで走れ！`,
-      夢: 'ドアの向こうに夢があるなら、ドアが開くまで叩き続けるんだ！',
-      挑戦: '最近勝ち組とか負け組みとか流行っているけど、スタート切っているかどうかがボクは大事だと思うけどね',
-      挫折: '最初、サンザンな目にあう。二度目、オトシマエをつける。三度目、余裕',
-      幸せ: `アー・ユー・ハッピー？ ${name}はすでにハッピーなはずだぜ`,
-      老い: '年とるってのは細胞が老けることであって、魂が老けることじゃない',
-      とくにない: 'ボクは別にいいんだけど、YAZAWAがなんて言うかな？'
+      仕事: 'やり続けることしかないということだけですよ。自分の仕事にオレ才能あるんだと\n',
+      お金: `${name}の一生かかって稼ぐ金、YAZAWAの2秒。そこんとこ、ヨロシク！\n`,
+      恋愛: `アイ・ラブ・ユー オーケー？言っちゃえよ${name}！\n`,
+      人生: `${name}の人生なんだから。てめぇで走れ！\n`,
+      夢: 'ドアの向こうに夢があるなら、ドアが開くまで叩き続けるんだ！\n',
+      挑戦: '最近勝ち組とか負け組みとか流行っているけど、スタート切っているかどうかがボクは大事だと思うけどね\n',
+      挫折: '最初、サンザンな目にあう。二度目、オトシマエをつける。三度目、余裕\n',
+      幸せ: `アー・ユー・ハッピー？ ${name}はすでにハッピーなはずだぜ\n`,
+      老い: '年とるってのは細胞が老けることであって、魂が老けることじゃない\n',
+      とくにない: 'ボクは別にいいんだけど、YAZAWAがなんて言うかな？\n'
     }
-    return obj[answer]
-  }
-
-  async displayEnter () {
-    console.log('XXXXXXXXXXXXXXXXXXXXXXXX')
-    process.stdin.resume()
-    new Promise(r => process.stdin.once('data', r))
-    .finally(() => process.stdin.pause())
+    return obj[advice]
   }
 
   displayLine () {
-    console.log('-------------------------------------------------------------')
-    // await typewriter(selectedAdvice)
-    // console.log('-------------------------------------------------------------')
+    console.log('-----------------------------------------------------------------')
   }
 
-  lastMessage () {
+  lastMessage (count) {
+    setTimeout(() => {
+    console.log('-----------------------------------------------------------------')
     console.log(
       '  ____    ___     ___    ____      _       _   _    ____   _  __ \n' +
       ' / ___|  / _ \\   / _ \\  |  _ \\    | |     | | | |  / ___| | |/ / \n' +
@@ -88,11 +81,12 @@ class Display {
       '| |_| | | |_| | | |_| | | |_| |   | |___  | |_| | | |___  | . \\ \n' +
       ' \\____|  \\___/   \\___/  |____/    |_____|  \\___/   \\____| |_|\\_\\'
     )
+    }, count)
   }
 
   typewriter (text) {
     return new Promise((resolve) => {
-      let speed = 50
+      let speed = this.time
       let displayText = text.split("")
       return displayText.forEach((char, index) => {
         setTimeout(() => {
@@ -102,30 +96,26 @@ class Display {
       })
     })
   }
-
-//   async displayText (text) {
-//     await typewriter(text)
-//     displayLine()
-//   }
 }
 
 class Main {
-  constructor() {
-    this.display = new Display()
+  constructor(time) {
+    this.display = new Display(time)
+    this.time = time
   }
 
   async run() {
     await this.display.firstMessage()
     const name = await this.display.userNameInput()
-    this.display.thankMessage(name)
+    await this.display.thankMessage(name)
     const selectedWorry = await this.display.selectWorry()
     const selectedAdvice = await this.display.selectAdvise(name, selectedWorry)
-    await this.display.displayEnter()
     await this.display.displayLine()
     await this.display.typewriter(selectedAdvice)
-    await this.display.lastMessage()
+    const count = this.time * selectedAdvice.length
+    await this.display.lastMessage(count)
   }
 }
 
-const main = new Main()
+const main = new Main(50)
 main.run()
